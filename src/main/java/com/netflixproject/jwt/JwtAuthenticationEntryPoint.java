eaf5b28b85277827fce9e7b6848eb3192867a0bf
SunYub <goggi772@gmail.com>
2023-07-06 17:17:58 +0900

    Modifying Login & Register API

--- src/main/java/com/netflixproject/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/netflixproject/jwt/JwtAuthenticationEntryPoint.java
@@ -6,9 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,19 +16,66 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private final HandlerExceptionResolver handlerExceptionResolver;
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        String exception = (String)request.getAttribute("exception");
+        ErrorCode errorCode;
+
+        log.debug("log: exception: {} ", exception);
+
+        /**
+         * 토큰 없는 경우
+         */
+        if(exception == null) {
+            errorCode = ErrorCode.LOGIN_FAILED;
+            setResponse(response, errorCode);
+            return;
+        }
+
+        /**
+         * 토큰 만료된 경우
+         */
+        if(exception.equals(ErrorCode.EXPIRED_TOKEN.getCode())) {
+            errorCode = ErrorCode.EXPIRED_TOKEN;
+            setResponse(response, errorCode);
+            return;
+        }
+
+        /**
+         * 토큰 시그니처가 다른 경우
+         */
+        if(exception.equals(ErrorCode.INVALID_JWT_TOKEN.getCode())) {
+            errorCode = ErrorCode.INVALID_JWT_TOKEN;
+            setResponse(response, errorCode);
+        }
+    }
+
+    /**
+     * 한글 출력을 위해 getWriter() 사용
+     */
+    private void setResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().println("{ \"message\" : \"" + errorCode.getMessage()
+                + "\", \"code\" : \"" +  errorCode.getCode()
+                + "\", \"status\" : " + errorCode.getStatus()
+                + ", \"errors\" : [ ] }");
+    }
+    /*@Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        log.error("Responding with unauthorized error. Message - {}", authException.getMessage());
-/*
-        ErrorCode unAuthorizationCode = (ErrorCode) request.getAttribute("unauthorization.code");
-
-        request.setAttribute("response.failure.code", unAuthorizationCode.getCode());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, unAuthorizationCode.getMessage());*/
-
-//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-        handlerExceptionResolver.resolveException(request, response, null, authException);
-
-    }
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException e) throws IOException, ServletException {
+        String noAuthMessage = "{\n" +
+                "    \"status\": \"FAILURE\",\n" +
+                "    \"message\": \"No authority\"\n" +
+                "}";
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(APPLICATION_JSON.toString());
+        response.getWriter().println(noAuthMessage);
+        resolver.resolveException(request, response, null, e);
+    }*/
 }
--- src/main/java/com/netflixproject/repository/MemberRepository.java
+++ src/main/java/com/netflixproject/repository/MemberRepository.java
@@ -15,10 +15,10 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
 
-    static final String UPDATE_USER_LAST_LOGIN = "UPDATE member SET LAST_LOGIN_TIME = :lastLoginTime WHERE EMAIL = :email";
+    /*static final String UPDATE_USER_LAST_LOGIN = "UPDATE member SET MODIFIED_TIME = :lastLoginTime WHERE USERNAME = :username";
 
     @Transactional //트랜잭션 설정(에러발생시 DB rollback)
     @Modifying //DB 테이블 변경하는 쿼리 작성시 적용해야 하는 annotation
     @Query(value = "UPDATE_USER_LAST_LOGIN", nativeQuery = true) //일반 sql쿼리 적용s
-    public int updateUserLastLogin(@Param("email") String email, @Param("lastLoginTime") LocalDateTime lastLoginTime);
+    public int updateUserLastLogin(@Param("username") String username, @Param("lastLoginTime") LocalDateTime lastLoginTime);*/
 }
